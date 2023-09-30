c113c3730c6a7e98cc56fbd918dce1e2e14e107e
Alexandre Junior <alexandrejunior408@gmail.com>
2023-09-29 23:15:24 -0300

    refactor: add customized error for missing param

--- src/presentation/controller/signup.controller.ts
+++ src/presentation/controller/signup.controller.ts
@@ -1,17 +1,18 @@
-import { HttpRequest, HttpResponse } from "../protocols/http"
+import { MissingParamError } from "../errors/missing-param.error"
+import { HttpRequest, HttpResponse } from "../protocols/http.protocol"
 
 export class SignUpController {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse | undefined> {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error("Missing param: name"),
+        body: new MissingParamError("name"),
       }
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error("Missing param: email"),
+        body: new MissingParamError("email"),
       }
     }
   }
--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,4 +1,5 @@
-import { SignUpController } from "./signup"
+import { MissingParamError } from "../errors/missing-param.error"
+import { SignUpController } from "./signup.controller"
 
 describe("SignUpController", () => {
   it("should return 400 if no name is provided", async () => {
@@ -12,7 +13,7 @@ describe("SignUpController", () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new Error("Missing param: name"))
+    expect(httpResponse?.body).toEqual(new MissingParamError("name"))
   })
 
   it("should return 400 if no email is provided", async () => {
@@ -26,6 +27,6 @@ describe("SignUpController", () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new Error("Missing param: email"))
+    expect(httpResponse?.body).toEqual(new MissingParamError("email"))
   })
 })
