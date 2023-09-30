15c100a33d890b799c111ade6a377003fefd1577
Alexandre Junior <alexandrejunior408@gmail.com>
2023-09-29 22:53:36 -0300

    feat: ensure SignUpController returns an error if no email is provided

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,10 +1,16 @@
 export class SignUpController {
   async handle(httpResponse: any): Promise<any> {
-    console.log(httpResponse)
-
-    return {
-      statusCode: 400,
-      body: new Error("Missing param: name"),
+    if (!httpResponse.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      }
+    }
+    if (!httpResponse.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      }
     }
   }
 }
