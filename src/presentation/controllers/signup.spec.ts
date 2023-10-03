93990759d2d3419e92fe65306131c4bebd0d66f9
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-02 22:37:54 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,6 @@
-import { InvalidParamError } from "../errors/invalid-param.error"
-import { MissingParamError } from "../errors/missing-param.error"
-import { ServerError } from "../errors/server.error"
-import { EmailValidator } from "../protocols/email-validator.protocol"
+import { InvalidParamError, MissingParamError, ServerError } from "../errors"
+import { EmailValidator } from "../protocols"
+
 import { SignUpController } from "./signup.controller"
 
 interface SutTypes {
