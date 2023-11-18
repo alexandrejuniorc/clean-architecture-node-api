4a4fea04d93f4626d6727e88d5b42f78db9ba18a
Alexandre Junior <alexandrejunior408@gmail.com>
2023-11-18 14:52:33 -0300

    feat: :sparkles: ensure BcryptAdapter calls with correct values

---
+++ src/infrastructure/criptography/bcrypt.adapter.ts
@@ -0,0 +1,16 @@
+import { Encrypter } from "@/data/protocols/encrypter.protocol"
+import * as bcrypt from "bcrypt"
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+  constructor(salt: number) {
+    this.salt = salt
+  }
+  async encrypt(value: string): Promise<string> {
+    return await bcrypt.hash(value, this.salt)
+  }
+
+  async compare(): Promise<boolean> {
+    return true
+  }
+}
