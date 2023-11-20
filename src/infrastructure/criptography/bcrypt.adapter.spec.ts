e33906118305d617d71c005fb64945ebf6628a4e
Alexandre Junior <alexandrejunior408@gmail.com>
2023-11-20 16:45:39 -0300

    test: :white_check_mark: ensure BcryptAdapter returns a hash on success

--- src/infrastructure/criptography/bcrypt.adapter.spec.ts
+++ src/infrastructure/criptography/bcrypt.adapter.spec.ts
@@ -1,5 +1,12 @@
 import { BcryptAdapter } from "./bcrypt.adapter"
 import * as bcrypt from "bcrypt"
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve("hash")
+  },
+}))
+
 describe("BcryptAdapter", () => {
   it("should call bcrypt with correct values", async () => {
     const salt = 12
@@ -7,5 +14,11 @@ describe("BcryptAdapter", () => {
     const hashSpy = jest.spyOn(bcrypt, "hash")
     await sut.encrypt("any_value")
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt)
+  })
+  it("should return a hash on success", async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt("any_value")
+    expect(hash).toBe("hash")
   })
 })
